bebdc942f002c3f0ee6ac27e62dff1997b8c523c
serginho <sergiodinizdemiranda@gmail.com>
2024-08-10 15:39:50 -0300

    implementação da atividade

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    static Integer numeroConta;
+    static String agencia;
+    static String nomeCliente;
+    static Double saldo;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in).useDelimiter("\n");;
+
+        System.out.println("Favor, digite o número da Agência: ");
+        agencia = sc.nextLine();
+        System.out.println("Favor, digite o número da Conta: ");
+        numeroConta = sc.nextInt();
+        System.out.println("Favor, digite o seu Nome: ");
+        nomeCliente = sc.next();
+        System.out.println("Favor, digite o Saldo: ");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente +
+                ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta "
+                + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
+
 }
